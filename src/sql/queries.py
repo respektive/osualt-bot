473fae0d1e0e0b191486b6359fafb51675899bcd
respektive <liambzw@gmail.com>
2023-04-26 18:58:12 +0200

    add -order agedscore to getscores

--- src/sql/queries.py
+++ src/sql/queries.py
@@ -313,6 +313,10 @@ async def get_beatmap_list(ctx, di, tables=None, sets=False, bonusColumn=None, m
             di["-order"] = "beatmaps.approved_date"
         if di["-order"] == "mods":
             di["-order"] = "enabled_mods"
+        if di["-order"] == "agedscore":
+            if not di.get("-direction") or di.get("-dir"):
+                di["-direction"] = "desc"
+            di["-order"] = "score * floor((DATE_PART('year', NOW() - approved_date) + DATE_PART('day', NOW() - approved_date) / 365.2425))"
         if di["-order"] == "lazerscore":
             if not di.get("-direction") or di.get("-dir"):
                 di["-direction"] = "desc"
@@ -467,6 +471,8 @@ async def get_beatmap_list(ctx, di, tables=None, sets=False, bonusColumn=None, m
                 formatBonusColumn = str(datetime.timedelta(seconds=int(b[6])))
             elif order == "enabled_mods":
                 formatBonusColumn = get_mods_string(b[6])
+            elif order == "score * floor((DATE_PART('year', NOW() - approved_date) + DATE_PART('day', NOW() - approved_date) / 365.2425))":
+                formatBonusColumn = "{:,.0f}".format(b[6])
             elif not str(b[6]).isnumeric():
                 formatBonusColumn = str(b[6])
             else:
