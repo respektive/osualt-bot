3df9ad86f21181f1b28cf538ea177718290c089c
respektive <liambzw@gmail.com>
2023-05-24 21:40:01 +0200

    update embed design

--- src/card/embed.py
+++ src/card/embed.py
@@ -2,13 +2,21 @@ import io
 import discord
 
 
-def get_card_embed(filepath):
-    embed = discord.Embed(colour=discord.Colour(0xcc5288))
+def get_card_embed(image, user_data, avatar_url):
+    author_string = f"""{user_data["username"]} - {user_data["pp"]:,}pp (#{user_data["global_rank"]:,}) ({user_data["country_code"]}#{user_data["country_rank"]:,})"""
 
-    with open(filepath, "rb") as file:
-        image_data = file.read()
+    embed = discord.Embed(colour=discord.Colour(0xCC5288))
+    embed.set_author(
+        name=author_string,
+        icon_url=avatar_url,
+        url=f"https://osu.ppy.sh/users/{user_data['user_id']}",
+    )
 
-    file = discord.File(io.BytesIO(image_data), filename="card.png")
+    image_data = io.BytesIO()
+    image.save(image_data, format="PNG")
+    image_data.seek(0)
+
+    file = discord.File(image_data, filename="card.png")
 
     embed.set_image(url="attachment://card.png")
 
