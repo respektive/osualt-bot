17730d42427afe6a94dc677736fcbd130404a6ac
respektive <respektivee@gmail.com>
2024-03-27 22:02:02 +0100

    ok only set with -nolist whatever

--- src/extensions/advanced.py
+++ src/extensions/advanced.py
@@ -105,7 +105,7 @@ class Advanced(commands.Cog):
             kwargs["-registered"] = "true"
 
         user_id = await get_user_id(ctx, kwargs)
-        if user_id is not None and not kwargs.get("-user"):
+        if user_id is not None and not kwargs.get("-user") and kwargs.get("-nolist"):
             kwargs["-user"] = user_id
 
         combo_score_multiplier = "500000"
